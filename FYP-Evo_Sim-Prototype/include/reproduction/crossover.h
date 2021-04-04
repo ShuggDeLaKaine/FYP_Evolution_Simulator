1f046001f297955036cae2237d03cebc3fcd5893
Tom Hodgson <tomhodgson55@gmail.com>
2021-04-04 17:19:55 +0100

    Continues implementation of Crossover. Completed crossoverGeneStacks() and updated fullCrossover(). Tested and works as effected.
    NEXT: bring in creature creation of the new offspring and species updating.

--- FYP-Evo_Sim-Prototype/include/reproduction/crossover.h
+++ FYP-Evo_Sim-Prototype/include/reproduction/crossover.h
@@ -2,6 +2,7 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include "core/generalFunctions.h"
 
 
 /*	\class Crossover
@@ -17,9 +18,10 @@ public:
 	
 	void getGeneStacks(std::vector<std::pair<float, std::vector<float>>> parents);		//!< get the gene stacks (will be from selection.h/cpp) and store in toPairList.
 	void pairParents(std::vector<std::pair<std::vector<float>, bool>>& parents);		//!< split chosen group into pair.
-	void crossoverGeneStacks(std::vector<std::pair<std::vector<float>, std::vector<float>>> pairParents);	//!< combine gene stacks of chosen parents.
+	void crossoverGeneStacks(std::pair<std::vector<float>, std::vector<float>> pairParents, uint32_t geneStackSize);	//!< combine gene stacks of chosen parents.
 private:
 	std::vector<std::pair<std::vector<float>, bool>> geneStacksToPairVec;				//!< vector to take the gene stacks that will be crossed over.
 	std::vector<std::pair<std::vector<float>, std::vector<float>>> pairedParents;		//!< vector of pairs, each a vector of floats, gene stack 1 and gene stack 2 from each parent.
-	std::vector<float> tempGeneStack;		//!< temp gene stack to contain the core gene values whilst an offspring is under construction.
+	std::vector<float> tempGeneStack;			//!< temp gene stack to contain the core gene values whilst an offspring is under construction.
+	std::shared_ptr<GeneralFunctions> genFunc;	//!< pointer ref to general functions class.
 };
--- FYP-Evo_Sim-Prototype/src/reproduction/crossover.cpp
+++ FYP-Evo_Sim-Prototype/src/reproduction/crossover.cpp
@@ -19,10 +19,17 @@ void Crossover::fullCrossover(std::vector<std::pair<float, std::vector<float>>> 
 	//pair these parents and their gene stacks up. 
 	pairParents(geneStacksToPairVec);
 
-	//cross over paired gene stacks into new gene stacks from offspring creatures.
-	crossoverGeneStacks(pairedParents);
+	//loop through paired parents.
+	for(int i = 0; i < pairedParents.size(); i++)
+	{
+		//cross over paired gene stacks into new gene stacks from offspring creatures.
+		crossoverGeneStacks(pairedParents.at(i), pairedParents.at(i).first.size());
 
-	//create these creatures and add to species.
+		//create these creatures and add to species.
+
+	}
+
+	
 
 
 	//clear up ... for next species.
@@ -44,7 +51,7 @@ void Crossover::pairParents(std::vector<std::pair<std::vector<float>, bool>>& pa
 	for (int i = 0; i < parents.size(); i++)
 	{
 		//check that the creature hasn't already been assigned, if their second pair is false.
-		if (parents.at(i).second != false)
+		if (parents.at(i).second == false)
 		{
 			//pair creature at i and i+1
 			pairedParents.push_back(std::make_pair(parents.at(i).first, parents.at(i + 1).first));
@@ -63,21 +70,22 @@ void Crossover::pairParents(std::vector<std::pair<std::vector<float>, bool>>& pa
 	}
 }
 
-void Crossover::crossoverGeneStacks(std::vector<std::pair<std::vector<float>, std::vector<float>>> pairParents)
+void Crossover::crossoverGeneStacks(std::pair<std::vector<float>, std::vector<float>> pairParents, uint32_t geneStackSize)
 {
+	//clear the temp geneStack, then reserve size.
+	tempGeneStack.clear();
+	tempGeneStack.reserve(geneStackSize);
+
 	//get a random uniform number from within the size of the geneStack vector.
-
-
-	//have a temporary vector to contain the geneStack whilst it is being constructed.
-
+	int randNum = genFunc->uniformIntBetween(0, geneStackSize);
 
 	//loop through parent1 geneStack, start at the beginning to end at the random number, assigning each gene element to the new offspring geneStack.
-
+	for (int i = 0; i < randNum; i++)
+		tempGeneStack.push_back(pairParents.first.at(i));
 
 	//loop through parent2 geneStack, start at the random number to end at the end, assigning each gene element to the new offsping geneStack.
-
-
-	//ensure that each element of the newly created gene stack has a value.
+	for (int i = randNum; i < geneStackSize; i++)
+		tempGeneStack.push_back(pairParents.second.at(i));
 
 
 }
